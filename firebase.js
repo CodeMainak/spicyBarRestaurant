26df681c84440d263e7eb94db508a4febc9f22d8
Mainak Mondal <mainak.mondal33@gmail.com>
2021-01-07 15:37:58 +0530

    firebase-added

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBzYOH5dugQfaBGi4KdNj5eE-t8U5eh3q8",
+  authDomain: "mcky-bar-restaurant.firebaseapp.com",
+  projectId: "mcky-bar-restaurant",
+  storageBucket: "mcky-bar-restaurant.appspot.com",
+  messagingSenderId: "988023659531",
+  appId: "1:988023659531:web:ae2324bbb5d4b66d76fb0e",
+  measurementId: "G-N9RGWWGCSL",
+};
